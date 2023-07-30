bac4d74bfa285d0382783f1e85abf44f6b1a05c7
astrojohnbon <53719274+astrojohnbon@users.noreply.github.com>
2023-07-30 14:21:29 -0700

    testing first upload

---
+++ Math.py
@@ -0,0 +1,10 @@
+#Let's make a robot barista bouncer
+
+print("Hello, Welcome to NetworkChuckCoffee!")
+
+name = input("What is your name? \n")
+
+if name == "Ben":
+    print("You're not welcome here EVIL BEN! Get out!")
+else: 
+    print("Hello " + name + ", thank you so much for coming in today")
